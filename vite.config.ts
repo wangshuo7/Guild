ebb03cfcc896531444d525870e8a68a11bc08940
wangshuo7 <13665439520@163.com>
2023-12-18 16:08:43 +0800

    feat：新增暗黑模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag: any) => tag.includes('session')
+        }
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
